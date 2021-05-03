8e7d25cab732dfe7859861055862c7d7f5e572b2
xtremeCode <abelkelly6022@gmail.com>
2021-05-03 21:59:19 +0100

    made uploads to the files

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function addTodo(e){
         let span2 = document.createElement('span')
         const img = document.createElement('img');
         img.classList.add('check')
-        img.src = 'file:///C:/Users/DELL/todo-app-main/images/icon-cross.svg'
+        img.src = 'images/icon-cross.svg'
         let li = document.createElement('li')
         let iTag = document.createElement('i')
         li.setAttribute('draggable', 'true')
@@ -211,51 +211,6 @@ function iconChange(e){
        comp.style.backgroundColor = 'white'
     }
 }
-
-
-
-
-
-// const draggables = document.querySelectorAll('.draggable');
-// const containers = document.querySelectorAll('.container');
-
-// draggables.forEach(draggable => {
-//     draggable.addEventListener('dragstart', () => {
-//         draggable.classList.add('dragging')
-//     })
-
-//     draggable.addEventListener('dragend', () => {
-//         draggable.classList.remove('dragging')
-//     })
-// })
-
-// containers.forEach(container => {
-//     container.addEventListener('dragover', e => {
-//         e.preventDefault()
-//         const afterElement = getDragAfterElement(container, e.clientY)
-//                 const draggable = document.querySelector('.dragging');
-//         if (afterElement == null) {
-//             container.appendChild(draggable)
-//         } else {
-//             container.insertBefore(draggable, afterElement)
-//         }
-
-//     })
-// })
-
-// function getDragAfterElement(container, y) {
-//     const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
-
-//     return draggableElements.reduce((closest, child) => {
-//         const box = child.getBoundingClientRect()
-//         const offset = y - box.top - box.height / 2
-//         if (offset < 0 && offset > closest.offset) {
-//             return { offset: offset, element: child }
-//         } else {
-//             return closest
-//         }
-//     }, { offset: Number.NEGATIVE_INFINITY }).element
-// }
 
 
 
